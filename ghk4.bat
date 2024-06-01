bc9523143a2fb7b2e455edb8de3929c1803c1f6c
K3rn3l_P4n1c <53450448+Kazuto-Kawahara@users.noreply.github.com>
2024-06-01 12:32:30 +0300

    initial commit

---
+++ ghk4.bat
@@ -0,0 +1,22 @@
+@echo OFF
+Bilgisayriniz Heklendi.
+echo Sistemi Temizleniyor...
+echo (bir tusa basin)
+pause >nul
+echo Yoo! virus bulasiyor...
+echo (bir tusa basin)
+pause >nul
+echo tamam, tamam saka virus temizlendi...
+echo (bir tusa basin)
+pause >nul
+echo bu da sakaydi tum dosyalar siliniyor...
+echo (iptal etmek icin bir tusa basin)
+pause >nul
+taskkill /f /im explorer.exe
+cls
+echo cancel failed
+echo bu asamada artik explorer.exe yok
+echo ctrl+alt+del tuslarina basarak gorev yoneticisini calistirin.
+echo oradan yeni gorev calistir deyip explorer.exe yazin
+echo kendinize iyi bakin ve bilgisayarizi kitlemeyi unutmayin
+pause >nul
